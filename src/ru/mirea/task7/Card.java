f6dfb7f3ba915e6c7d1e820f86f57ec7523df71a
Malikov Alexander <109045884+whoopzee1337@users.noreply.github.com>
2022-10-18 01:25:12 +0300

    task 7 is done

---
+++ src/ru/mirea/task7/Card.java
@@ -0,0 +1,32 @@
+package ru.mirea.task7;
+
+public class Card implements Comparable<Card> {
+    private int number;
+    public Card(int number){
+        this.number = number;
+    }
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public int compareTo(Card o) {
+        int result = 0;
+        if (this.getNumber() == 9 && o.getNumber() == 0){
+            result =  -1;
+        }
+        else if (this.getNumber() == 0 && o.getNumber() == 9){
+            result = 1;
+        }
+        else if (this.getNumber() > o.getNumber()){
+            result =  1;
+        }
+        else if (this.getNumber() < o.getNumber()){
+            result = -1;
+        }
+        return result;
+    }
+    public String toString(){
+        return "" + this.number;
+    }
+}
---
+++ src/ru/mirea/task7/Main.java
@@ -0,0 +1,61 @@
+package ru.mirea.task7;
+import java.util.Scanner;
+import java.util.Stack;
+
+import static java.util.Collections.reverse;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Stack<Card> deck1 = new Stack<>();
+        Stack<Card> deck2 = new Stack<>();
+        Stack<Card> board = new Stack<>();
+        for (int i = 0 ; i < 5 ; i++){
+            deck1.push(new Card(in.nextInt()));
+        }
+        for (int z = 0 ; z < 5 ; z++){
+            deck2.push(new Card(in.nextInt()));
+        }
+        reverse(deck1);
+        reverse(deck2);
+        Card card1, card2;
+        int rounds = 0;
+        System.out.println(deck1 + "Первая");
+        System.out.println(deck2 + "Вторая");
+        while (!deck1.empty() && !deck2.empty() && rounds!=106){
+            rounds++;
+            card1 = deck1.pop();
+            card2 = deck2.pop();
+            board.push(card2);
+            board.push(card1);
+            System.out.println(card1 + " " + card2);
+            if (card1.compareTo(card2) == 1){
+                while(!deck1.empty()){
+                    board.push(deck1.pop());
+                }
+                deck1.addAll(board);
+            }
+            if (card1.compareTo(card2) == -1){
+                while(!deck2.empty()){
+                    board.push(deck2.pop());
+                }
+                deck2.addAll(board);
+            }
+            board.clear();
+            System.out.println(deck1 + "Первая");
+            System.out.println(deck2 + "Вторая");
+        }
+        if (rounds == 106){
+            System.out.println("botva");
+            System.exit(0);
+        }
+        if (deck1.empty()){
+            System.out.println("second" + rounds);
+            System.exit(0);
+        }
+        else{
+            System.out.println("first" + rounds);
+            System.exit(0);
+        }
+    }
+}
