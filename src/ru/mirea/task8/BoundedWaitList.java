35c37cb1b46a4a4b64042ccd54c6ce4490968a78
Malikov Alexander <109045884+whoopzee1337@users.noreply.github.com>
2022-11-01 13:56:14 +0300

    practice8

---
+++ src/ru/mirea/task8/BoundedWaitList.java
@@ -0,0 +1,15 @@
+package ru.mirea.task8;
+
+public class BoundedWaitList<E>{
+    private int capacity;
+    public BoundedWaitList(int capacity){
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+    public void add (E element){
+
+    }
+}
